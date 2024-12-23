0d584d15c42a35019a8ea96fdb48178cc73a048d
Bvaishnavi123 <bajajvaishnavi1999@gmail.com>
2024-12-23 22:50:16 +0530

    Added vision changes

--- components/sections/vision/index.jsx
+++ components/sections/vision/index.jsx
@@ -1,18 +1,15 @@
 "use client";
 
-import { VisionContainer } from './vision-container';
-import { VisionHeading } from './vision-heading';
-import { VisionText } from './vision-text';
+import { VisionContainer } from "./vision-container";
+import { VisionHeading } from "./vision-heading";
+import { VisionText } from "./vision-text";
 
 export function VisionSection() {
   return (
     <section className="relative py-16 md:py-14 bg-white">
       <VisionHeading />
       <VisionContainer>
-        
-       
-        <div className="space-y-8 md:space-y-16">
-          
+        <div className="space-y-9 md:space-y-18">
           <VisionText />
         </div>
       </VisionContainer>
