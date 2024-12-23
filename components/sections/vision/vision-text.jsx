64b044b52802a358466aed5a0a941e551a946cd1
Muzammil Ahmed <muzammilahmed.166@gmail.com>
2024-12-23 21:44:14 +0530

    Image bug fix

--- components/sections/vision/vision-text.jsx
+++ components/sections/vision/vision-text.jsx
@@ -3,7 +3,7 @@
 import { motion } from 'framer-motion';
 import { VisionWord } from './vision-word';
 import { VisionImage } from './vision-image';
-
+import Image from 'next/image';
 export function VisionText() {
   return (
     <div className="text-justify mx-auto max-w-full md:max-w-[850px]">
