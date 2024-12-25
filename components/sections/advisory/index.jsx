c3a079237387984b81faf197df5fc8ea5b5d75ff
shaikmohammed9009 <72823661+shaikmohammed9009@users.noreply.github.com>
2024-12-25 16:37:03 +0530

    initial commit

--- components/sections/advisory/index.jsx
+++ components/sections/advisory/index.jsx
@@ -10,7 +10,7 @@ export function AdvisorySection() {
     <section className="bg-white py-24">
       <AdvisoryContainer>
         <AdvisoryHeader />
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center mt-16">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 items-center mt-16">
           <div className="space-y-8">
             <AdvisoryContent />
           </div>
